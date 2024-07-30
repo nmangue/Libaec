2d0799cd752845cb906a473344e5895c97fbfd0f
nmangue <nmangue@users.noreply.github.com>
2024-07-30 21:29:01 +0200

    Declare interop classes

---
+++ src/Libaec/AecDataFlags.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Libaec;
+
+/// <summary>
+/// Sample data description flags
+/// </summary>
+[Flags]
+public enum AecDataFlags
+{
+    /// <summary>
+    /// Samples are signed. Telling libaec this results in a slightly better compression ratio. Default is unsigned.
+    /// </summary>
+    AEC_DATA_SIGNED = 1,
+
+    /// <summary>
+    /// 24 bit samples are coded in 3 bytes
+    /// </summary>
+    AEC_DATA_3BYTE = 2,
+
+    /// <summary>
+    /// Samples are stored with their most significant bit first. This has nothing to do with the endianness of the host. Default is LSB.
+    /// </summary>
+    AEC_DATA_MSB = 4,
+
+    /// <summary>
+    /// Set if preprocessor should be used
+    /// </summary>
+    AEC_DATA_PREPROCESS = 8,
+
+    /// <summary>
+    /// Use restricted set of code options
+    /// </summary>
+    AEC_RESTRICTED = 16,
+
+    /// <summary>
+    /// Pad RSI to byte boundary. Only used for decoding some CCSDS sample data. Do not use this to produce new data as it violates the standard.
+    /// </summary>
+    AEC_PAD_RSI = 32,
+
+    /// <summary>
+    /// Do not enforce standard regarding legal block sizes.
+    /// </summary>
+    AEC_NOT_ENFORCE = 64
+}
